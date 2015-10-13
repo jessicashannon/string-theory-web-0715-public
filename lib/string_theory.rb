05012ab4b1ce267b99a29810cadae9c46ac1aba1
jessicashannon <jessdahling@gmail.com>
2015-10-13 13:57:28 -0400

    done

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def sort_by_appearance(characters_to_sort, long_text)
-  # code goes here
+  char_array = characters_to_sort.split("")
+  char_hash = {}
+  char_array.each do |char|
+    char_hash[char] = 0
+  end
+  text_array = long_text.split("")
+  text_array.each do |letter|
+    if char_hash.key?(letter)
+      char_hash[letter] += 1
+    end
+  end
+  sorted_hash = char_hash.sort_by{ |key, value| value } #sort_by returns an array of two element arrays
+  sorted_hash.map{|array| array[0]}.join("").reverse
 end
